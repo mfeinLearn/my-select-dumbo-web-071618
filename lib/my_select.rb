44f6e2cfe40bb59c251791242fc9e05697fb4c19
Malcome Fein <mfein90@gmail.com>
2018-08-03 12:49:31 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def my_select(collection)
  # your code here!
+   i = 0
+  new_array = []
+  
+  if collection.length > 0
+  while i < collection.length
+    if yield(collection[i])
+        new_array << collection[i]
+      end
+      i = i + 1
+    #binding.pry
+  end 
+else
+  puts "Empty collection"
+end 
+  new_array
 end
