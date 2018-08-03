e250830d9a57addb7db463b404d7080f674c50be
Malcome Fein <mfein90@gmail.com>
2018-08-03 12:44:16 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def my_select(collection)
  # your code here!
+   i = 0
+  answer = []
+  
+  while i < collection.size
+  
+      answer << yield(collection[i])
+      i = i + 1
+    #binding.pry
+  end 
+  answer
 end
